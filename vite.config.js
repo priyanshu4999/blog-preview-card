764033fbbd5206e468688814f42874f46274d050
payne <4999payne@gmail.com>
2025-07-05 21:50:54 +0530

    updated vitconfig

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './blog-preview-card-main/', // Use '/' for root hosting, or '/repo-name/' for GitHub Pages
+  base: '/', // Use '/' for root hosting, or '/repo-name/' for GitHub Pages
 });
